cc04c7f12414f72b0a91bbb8a153af3ed2797bd2
Bold MIU <hi_u_baina@yahoo.com>
2021-09-30 22:14:22 -0500

    WalMart
    Uploaded

---
+++ app/src/main/java/com/example/walmart/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.walmart
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun openUrl(view: View) {
+        val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.walmart.com/account/login"))
+        startActivity(browserIntent);
+    }
+
+}
